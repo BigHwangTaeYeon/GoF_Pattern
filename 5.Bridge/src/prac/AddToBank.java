0f0c49882456437720d2d7d7022fcc630f80a083
bighwang <gxd0214@gmail.com>
2023-11-17 12:47:54 +0900

    Adaptor complete, Bridge 아이디어 고갈

---
+++ 5.Bridge/src/prac/AddToBank.java
@@ -0,0 +1,30 @@
+package prac;
+
+public class AddToBank extends Bank {
+    private String fund;
+    private Bank fundBak;
+
+    public AddToBank(int balance, String fund) {
+        super(balance);
+        this.fund = fund;
+        this.fundBak = new Bank(0);
+    }
+    
+    public void fundWork(BankOperInterface bankInterface){
+        switch (fund) {
+            case "A":
+                fundBak.work(bankInterface, balance);
+                break;
+            case "B":
+                fundBak.work(bankInterface, balance);
+                break;
+            case "C":
+                fundBak.work(bankInterface, balance);
+                break;
+            default:
+                System.out.println("없는 상품입니다.");
+                break;
+        }
+    }
+    
+}
